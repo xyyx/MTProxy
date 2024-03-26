b5961a43a4b61591c41f3524563a5e8a48d65368
David Roman <davidroman96@gmail.com>
2024-03-27 00:50:03 +0100

    change version number to match next patch version

--- mtproto/mtproto-proxy.c
+++ mtproto/mtproto-proxy.c
@@ -68,7 +68,7 @@
 #define COMMIT "unknown"
 #endif
 
-#define VERSION_STR	"mtproxy-0.01"
+#define VERSION_STR	"mtproxy-3.0.5"
 const char FullVersionStr[] = VERSION_STR " compiled at " __DATE__ " " __TIME__ " by gcc " __VERSION__ " "
 #ifdef __LP64__
   "64-bit"
